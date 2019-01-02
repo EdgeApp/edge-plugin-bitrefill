ac2bcd9eac561f502296252e3a19608a6aeff44d
ahujsak <allen@missionvi.com>
2019-01-02 15:59:54 -0500

    api clean up

--- src/api.js
+++ src/api.js
@@ -7,12 +7,9 @@
 
 export const PROVIDER = 'edge'
 export const API_VERSION = '1'
-export const ACCEPT_LANGUAGE = 'en-US;q=0.7,en;q=0.3'
-export const HTTP_ACCEPT = 'en-US;q=0.7,en;q=0.3'
-export const RETURN_URL = 'https://simplex-api.edgesecure.co/redirect/'
 export const BITREFILL_URL = 'https://www.bitrefill.com/'
 export const API_KEY = 'APIKEYforEdge'
-export const CALLBACK_KEY = 'bitRefillBuy'
+export const CALLBACK_KEY = 'edge'
 export const LIMITS = {
   USD: {
     min: 50,
